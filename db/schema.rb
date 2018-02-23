e0733c33e194cca0d100195a7c7468127df0d4a2
Paul Tashman <paultashman@gmail.com>
2018-02-22 23:32:13 -0500

    Adds space scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180223042641) do
+ActiveRecord::Schema.define(version: 20180223043118) do
 
   create_table "discs", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 20180223042641) do
   end
 
   create_table "players", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "spaces", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
